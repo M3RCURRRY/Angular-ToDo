121a110da1c7eefce3d1456000f0e4a517f67fa4
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-12-07 22:55:44 +0300

    Connect HttpClient and JWT Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { TodoTaskCreatorComponent } from './components/todo-task-creator/todo-ta
 import { FilterPipe } from './filter.pipe';
 import { TodoTaskFilterComponent } from './components/todo-task-filter/todo-task-filter.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TodoPageComponent } from './pages/todo-page/todo-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './shared/interceptors/jwt.interceptor';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -18,15 +24,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     TodoListComponent,
     TodoTaskCreatorComponent,
     FilterPipe,
-    TodoTaskFilterComponent
+    TodoTaskFilterComponent,
+    TodoPageComponent,
+    AboutPageComponent,
+    AuthPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
